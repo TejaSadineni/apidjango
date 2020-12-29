89a4d20e69c0acea6f6aa1e693531fae17684477
TejaSadineni <46244941+TejaSadineni@users.noreply.github.com>
2020-12-29 22:44:22 +0530

    restframe work api

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10,decimal_places=2,default=0)
+    published_date = models.DateField()
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='covers/')
+    created_date = models.DateTimeField(auto_now_add=True)
--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class Another(View):
+    def get(self,request):
+        return HttpResponse("okok")
+
+
+
+def home(reques):
+    return HttpResponse("hello")
